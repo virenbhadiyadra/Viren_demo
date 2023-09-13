f1a8b49cc68ea7244f4f6c8d35c390f015e79e2c
virenbhadiyadra <122574600+virenbhadiyadra@users.noreply.github.com>
2023-09-12 23:11:50 -0700

    associative arrays

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php 
 $name  = 'Viren';
 $price = 2;
+/*associative arrays*/
+$nutrition = [
+    'fat'   => 38, 
+    'sugar' => 51, 
+    'salt'  => 0.25,
+    
+  ];
+    $nutrition['fat']   = 36;
+    $nutrition['fiber'] = 2.1;
+    $nutrition['protien'] = 7.3;
+  /* */
 ?>
 <!DOCTYPE html>
 <html>
@@ -13,5 +24,12 @@ $price = 2;
     <h2>Welcome <?php echo $name; ?></h2>
     <p>The cost of your candy is 
        $<?php echo $price; ?> per pack.</p>
+
+       <h2>Nutrition (per 100g)</h2>
+    <p>Fat:   <?php echo $nutrition['fat']; ?>%</p>
+    <p>Sugar: <?php echo $nutrition['sugar']; ?>%</p>
+    <p>Salt:  <?php echo $nutrition['salt']; ?>%</p>
+    <p>Fiber:  <?php echo $nutrition['fiber']; ?>%</p>
+    <p>Protien:  <?php echo $nutrition['protien']; ?>%</p>
   </body>
 </html>
