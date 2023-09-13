75eada1452ab0107f067d3d1bf4103ac718862e5
virenbhadiyadra <122574600+virenbhadiyadra@users.noreply.github.com>
2023-09-12 23:24:06 -0700

    arithmetic operators

--- index.php
+++ index.php
@@ -30,6 +30,14 @@ $nutrition = [
     /*echo shorthand*/ 
   $name      = 'Viren';
   $favorites = ['Sour Candies', 'jelly Candies', 'Coffee',];
+
+  /*Arithmetic operations*/
+$items    = 15;
+$cost     = 10;
+$subtotal = $cost * $items;
+$tax      = ($subtotal / 100) * 20;
+$total    = $subtotal + $tax;
+
   
 ?>
 <!DOCTYPE html>
@@ -73,6 +81,14 @@ $nutrition = [
       <h2>Welcome <?= $name ?></h2>
     <p>Your favorite type of candy is:
        <?= $favorites[0] ?>.</p>
+
+       <h2>Shopping Cart</h2>
+    <p>Items: <?= $items ?></p>
+    <p>Cost per pack: $<?= $cost ?></p>
+    <p>Subtotal: $<?= $subtotal ?></p>
+    <p>Tax: $<?= $tax ?></p>
+    <p>Total: $<?= $total ?></p>
+
 
   </body>
 </html>
