097691560c3d7419d6025b62f9f73d2f35381783
virenbhadiyadra <122574600+virenbhadiyadra@users.noreply.github.com>
2023-09-12 23:16:10 -0700

    Index arrays

--- index.php
+++ index.php
@@ -15,6 +15,9 @@ $nutrition = [
     $name  = 'Guest';
     $name  = 'Ivy';
     $price = 5;
+    /* Index arrays */
+    $best_sellers = ['Chocolate', 'Mints', 'Fudge', 'Licorice',
+    'Bubble gum', 'Toffee', 'Jelly beans',];
 ?>
 <!DOCTYPE html>
 <html>
@@ -34,5 +37,14 @@ $nutrition = [
     <p>Salt:  <?php echo $nutrition['salt']; ?>%</p>
     <p>Fiber:  <?php echo $nutrition['fiber']; ?>%</p>
     <p>Protien:  <?php echo $nutrition['protien']; ?>%</p>
+
+    <h2>Best Sellers</h2>
+    <ul>
+      <li><?php echo $best_sellers[0]; ?></li>
+      <li><?php echo $best_sellers[1]; ?></li>
+      <li><?php echo $best_sellers[2]; ?></li>
+      <li><?php echo $best_sellers[3]; ?></li>
+      <li><?php echo $best_sellers[4]; ?></li>
+    </ul>
   </body>
 </html>
