676c3967e7755358b4476981c3c75759541a2b48
virenbhadiyadra <122574600+virenbhadiyadra@users.noreply.github.com>
2023-09-12 23:28:00 -0700

    comparison operator

--- index.php
+++ index.php
@@ -43,6 +43,12 @@ $prefix  = 'Thank you';
 $name    = 'Viren';
 $message = "$prefix  $name";
 
+/* comparison operators */
+$item    = 'Chocolate';
+$stock   = 8;
+$wanted  = 5;
+$can_buy = ($wanted <= $stock);
+
   
 ?>
 <!DOCTYPE html>
@@ -96,6 +102,12 @@ $message = "$prefix  $name";
 
     <h2><?= $name ?>'s Order</h2>
     <p><?= $message ?></p>
+
+    <h2>Shopping Cart</h2>
+    <p>Item:    <?= $item ?></p>
+    <p>Stock:   <?= $stock ?></p>
+    <p>Wanted:  <?= $wanted ?></p>
+    <p>Can buy: <?= $can_buy ?></p>
 
 
   </body>
